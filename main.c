de80552e564788c9fc7180d77875757b4d9519cd
chemai_renny <94364645+chemaiRenny@users.noreply.github.com>
2022-01-18 17:20:45 +0300

    main file
    combination of hex-encode and decompress

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(int argc,char **argv)
         printf("Usage:\n\tmain <hex-encoded-compressed string>");
         exit(EXIT_FAILURE);
     }
-    char *input = "1f8b080032f7d85f02ff55cac10980400c04c056b602abb18153721a249b2309dabe873fbfc3aca760932c0c89ee618dbb406d84df62c282266a9e8ac6d452277ab87d46e7e371290f64b59ac93fffe1f202fcefa41e62000000";
+    char *input = argv[1];
 
     char *decoded=decode(input).buf;
 
